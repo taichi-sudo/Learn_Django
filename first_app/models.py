2ef1a77915bf4db821463e089401a3c1ba677846
taichi-sudo <taichi.fujiwara@one-star.jp>
2024-03-16 23:20:18 +0900

    modelの勉強
    modelのテスト

--- first_app/models.py
+++ first_app/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateField(auto_now_add=True, blank=True)
+    title = models.CharField(max_length=100, verbose_name = 'タイトル')
+    content = models.TextField(verbose_name = '本文')
+    created_at = models.DateField(auto_now_add=True, blank=True, verbose_name = '投稿日')
 
     def __str__(self):
         return self.title
     
     class Meta:
         verbose_name_plural = '記事'
+
+class Comment(models.Model):
+    content = models.TextField(verbose_name = '本文')
+    created_at = models.DateField(auto_now_add=True, blank=True, verbose_name = '投稿日')
+    article = models.ForeignKey(to=Article, related_name='comments', on_delete=models.CASCADE, verbose_name='記事')
+
+    def __str__(self):
+        return self.content
+    
+    class Meta:
+        verbose_name_plural = 'コメント'
